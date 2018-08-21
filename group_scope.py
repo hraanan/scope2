3b4bc34d0eec26e5a4eaad9a7ef084433e3a4d95
hraanan <hraanan@gmail.com>
2018-08-21 14:18:26 -0400

    push

--- group_scope.py
+++ group_scope.py
@@ -12,11 +12,11 @@ in_file_name="merge_metal_organic_chl_ca_10_md_3_rmsd_5_factor_correct_EC_1.4_1.
 in_file=open(in_file_name,"r")
 in_file.readline()
 #groups_file=open('groups_description_merge_metal_organic_chl_ca_10_md_3_rmsd_5_factor_1-4_1-3_1-15.csv','r')
-out_file=open('f:/scope2/groups_scope.txt','w')
+out_file=open('f:/scope2/groups_with_scope.txt','w')
 #out_file.write(groups_file.readline())
 
 #in_file.readline()
-out_file.write('ID\tscope\n')
+
 
 microen_groups={}
 
@@ -25,7 +25,7 @@ for line in in_file:
     line=line.split('\t')
     microen_groups[line[0]]=line[1]
 group_scope_dict={}
-scope_file=open('f:/scope2/microenvironments_scope_test.txt','r')
+scope_file=open('f:/scope2/microenvironments_scope.txt','r')
 scope_file.readline()
 for line in scope_file:
     line=line.split('\t')
@@ -36,107 +36,32 @@ for line in scope_file:
     scope=group_scope_dict.get(group)
     scope.append(line[1][:-1])
     group_scope_dict[group]=list(set(scope))
-
-for grp,scope in group_scope_dict.items():
-    out_file.write(grp+'\t'+';'.join(scope)+'\n')
+group_file=open('F:\Dropbox\postdoc\Results\Organic_cofactors\groups_description_merge_metal_organic_chl_ca_10_md_3_rmsd_5_factor_1-4_1-3_1-15_with_ECs.csv','r')
+titel=str(group_file.readline())[:-1]
+out_file.write(titel+'\t'+'scope\n')
+for line in group_file:
+    group=line.split('\t')[0]
+    scope=group_scope_dict.get(group)
+    
+    try:
+        
+        if len(scope)>1:
+            #print(len(scope))
+            line=line.split('\t')
+            
+            i=0
+            for i,scp in enumerate(scope):
+                print(i)
+                line[0]=group+'_'+str(i)
+                out_file.write('\t'.join(line)[:-1]+'\t'+scp+'\n')
+        else:
+            out_file.write(line[:-1]+'\t'+scp+'\n')
+    except TypeError:
+        continue
+#for grp,scope in group_scope_dict.items():
+#    out_file.write(grp+'\t'+';'.join(scope)+'\n')
 
 out_file.close()
 print('end')
 
 
-
-
-
-
-
-
-#x=0
-#y=0
-
-#groups={}
-#for line in in_file:
-#    ecs=[]
-#    line=line.split("\t")
-#    groups.setdefault(line[1],[])#.append(line[5])
-#    ecs=groups.get(line[1])
-#    if line[5] not in ecs:
-#        if line[5]=="-":
-#            continue
-#        ecs.append(line[5])
-        
-        #groups.setdefault(line[1],[]).append(prot_meta_dict.get(line[5]))
-#for key,value in groups.items():
-#    groups[key]=list(set(value))              
-#    
-
-#for line in in_file:
-#    line=line.split("\t")
-#    in_prot=line[0][0:4]
-#    #in_prot=in_prot[0]
-#    groups_list=[]
-#    metabolism_file=open(metabolism_file_name,'r')
-#    x=x+1    
-#    for metline in metabolism_file:
-#        metline=metline.split("\t")
-#        prot=metline[2].lower()
-#              
-#        if prot==in_prot:
-#            #line='\t'.join(line)
-#            y=y+1            
-#            
-#            out_file.write('\t'.join(line)[:-1]+'\t'+metline[0]+'\n')
-#out_file.close()
-#
-#max_age=0  
-#in_file=open('metabolism_'+in_file_name,"r")
-#
-#groups={}
-#in_file.readline()
-#for line in in_file:
-#    line=line.split("\t")
-#    groups.setdefault(line[1],[]).append(line[24][:-1])    
-
-#for key,value in groups.items():
-#    groups[key]=list(set(value))        
-#for line in groups_file:
-#    line=line.split("\t")
-#    #print(len(groups.get(line[0])))
-#    #print(groups.get(line[0]))
-#    ecs_tree=[]
-#    ecs_number=0
-#    grplen=0
-#    try:
-#        grplen=(len(groups.get(line[0])))
-#    except:
-#        groups[line[0]]=""
-#    if groups.get(line[0])!="":
-#        for ec in groups.get(line[0]):
-#           if '-' not in ec:
-#               ecs_number=ecs_number+1
-#               #ec=ec.split('.')
-#               ecs_tree.append(ec)
-#               ec=ec[:-2]
-#               if ec not in ecs_tree:
-#                   ecs_tree.append(ec)
-#               ec=ec[:-2]
-#               if ec not in ecs_tree:
-#                   ecs_tree.append(ec)
-#               ec=ec[:-2]
-#               if ec not in ecs_tree:
-#                   ecs_tree.append(ec)
-#               
-#        for ec in groups.get(line[0]):  
-#           if '-' in ec:
-#               ec=ec[:-2]
-#           if '-' in ec:
-#               ec=ec[:-2]
-#           if '-' in ec:
-#               ec=ec[:-2]
-#           if ec not in ecs_tree:
-#              ecs_tree.append(ec)
-#              ecs_number=ecs_number+1
-#               
-##        groups[line[0]]=""
-#    out_file.write('\t'.join(line)[:-1]+'\t'+';'.join(groups.get(line[0]))+'\t'+str(grplen)+'\t'+str(ecs_number)+'\n')
-#out_file.close()
-#print('end')
--- scope_distances.py
+++ scope_distances.py
@@ -5,16 +5,15 @@ Created on Fri Aug 17 11:43:37 2018
 @author: hraanan
 """
 
-#import community
-#import networkx as nx
-#
-#G=nx.Graph()
+import numpy
 
-out_file=open('f:/scope2/groups_scope_ditances_matrix.txt','w')
-out_file.write('source\ttarget\tdistance\n')
+
+out_file=open('f:/scope2/groups_scope_name_list.txt','w')
+out_file.write('group number\tgroup name\n')
 in_file=open('f:/scope2/groups_scope.txt','r')
 in_file.readline()
 scope_dict={}
+
 for line in in_file:
     line=line.split('\t')
     group = line[0]
@@ -22,28 +21,43 @@ for line in in_file:
     scope_list=line[1].split(';')
     if len(scope_list)==1:
         scope_dict[line[0]]=line[1]
-        continue
-    for i,scope in enumerate(scope_list):
-        group = line[0]+'_'+str(i)
-        scope_dict[group]=scope 
-i=0
-j=0
-
-for grp_1,scope_1 in scope_dict.items():
-    scope_1=scope_1.split('.')
-    for grp_2,scope_2 in scope_dict.items():
-        if j<i:
-            continue
         
-        scope_2=scope_2.split('.')
-        dis=0
-        if scope_1[0]==scope_2[0]: dis=1  
-        if scope_1[0]==scope_2[0] and scope_1[1]==scope_2[1]:dis=2
-        if scope_1[0]==scope_2[0] and scope_1[1]==scope_2[1] and scope_1[2]==scope_2[2]:dis=3
-        if scope_1[0]==scope_2[0] and scope_1[1]==scope_2[1] and scope_1[2]==scope_2[2] and scope_1[3]==scope_2[3]:dis=4
-        out_file.write(grp_1+'\t'+grp_2+'\t'+'.'.join(scope_1)+'\t'+'.'.join(scope_2)+'\t'+str(dis)+'\n')
-        j=j+1
+        continue
+    for t,scope in enumerate(scope_list):
+        group = line[0]+'_'+str(t)
+        scope_dict[group]=scope 
+        
+
+
+
+i=1
+j=0
+for grp,scp in scope_dict.items():
+    out_file.write(str(i)+'\t'+grp+'\n')
     i=i+1
+
+#scope_matrix = numpy.zeros(shape=(440,440))
+#
+#i=1
+#j=1
+#for grp_1,scope_1 in scope_dict.items():
+#    
+#    scope_1=scope_1.split('.')
+#    for grp_2,scope_2 in scope_dict.items():
+##        if j<i:
+##            continue
+#        #print (grp_1,grp_2)
+#        scope_2=scope_2.split('.')
+#        dis=0
+#        if scope_1[0]==scope_2[0]: dis=1  
+#        if scope_1[0]==scope_2[0] and scope_1[1]==scope_2[1]:dis=2
+#        if scope_1[0]==scope_2[0] and scope_1[1]==scope_2[1] and scope_1[2]==scope_2[2]:dis=3
+#        if scope_1[0]==scope_2[0] and scope_1[1]==scope_2[1] and scope_1[2]==scope_2[2] and scope_1[3]==scope_2[3]:dis=4
+#        #out_file.write(grp_1+'\t'+grp_2+'\t'+'.'.join(scope_1)+'\t'+'.'.join(scope_2)+'\t'+str(dis)+'\n')
+#        scope_matrix[i][j]=dis
+#        j=j+1
+#    j=0
+#    i=i+1
 
 
 print('end')
